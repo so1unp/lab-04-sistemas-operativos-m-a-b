19ef4f5db0eee09b384759f8ad2d2fa5c772498f
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2025-05-14 15:13:46 -0300

    Ejercicio 1 terminado

--- semaphores.c
+++ semaphores.c
@@ -1,6 +1,13 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <string.h>
+#include <fcntl.h>
+
+// Abrir/crear un semáforo
+sem_t *sem_operation(char *name, int mode, int value);
 
 void usage(char *argv[])
 {
@@ -16,36 +23,87 @@ void usage(char *argv[])
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2) {
+    if (argc < 2)
+    {
         usage(argv);
         exit(EXIT_FAILURE);
     }
 
-    if (argv[1][0] != '-') {
+    if (argv[1][0] != '-')
+    {
         usage(argv);
         exit(EXIT_FAILURE);
     }
 
     char option = argv[1][1];
 
-    switch(option) {
-        case 'c':
-            break;
-        case 'u':
-            break;
-        case 'd':
-            break;
-        case 'b':
-            break;
-        case 'i':
-            break;
-        case 'h':
-            usage(argv);
-            break;
-        default:
-            fprintf(stderr, "Opción desconocida: %s\n", argv[1]);
+    // Variable para el semáforo
+    sem_t *sem;
+
+    switch (option)
+    {
+    // Crear un semáforo
+    case 'c':
+        /* Crea el semáforo nombrado y luego lo cierra */
+        sem_close(sem_operation(argv[2], O_CREAT, atoi(argv[3])));
+        printf("Semáforo %s creado con valor inicial %s\n", argv[2], argv[3]);
+        break;
+    case 'u':
+        /* Abro el semaforo */
+        sem = sem_operation(argv[2], O_RDWR, 0);
+        sem_post(sem);
+        printf("UP en el semáforo %s\n", argv[2]);
+        /* Cierro el semáforo */
+        sem_close(sem);
+        break;
+    case 'd':
+        /* Abro el semáforo */
+        sem = sem_operation(argv[2], O_RDWR, 0);
+        printf("DOWN en el semáforo %s\n", argv[2]);
+        sem_wait(sem);
+        /* Cierro el semáforo */
+        sem_close(sem);
+        break;
+    case 'b':
+        /* Elimina el semáforo */
+        if (sem_unlink(argv[2]) == -1)
+        {
+            perror("Error al eliminar el semáforo");
             exit(EXIT_FAILURE);
+        }
+        printf("Semáforo %s eliminado\n", argv[2]);
+        break;
+    case 'i':
+        /* Abro el semáforo */
+        sem = sem_operation(argv[2], O_RDWR, 0);
+        int value;
+        if (sem_getvalue(sem, &value) == -1)
+        {
+            perror("Error al obtener el valor del semáforo");
+            exit(EXIT_FAILURE);
+        }
+        printf("Valor del semáforo %s: %d\n", argv[2], value);
+        /* Cierro el semáforo */
+        sem_close(sem);
+        break;
+    case 'h':
+        usage(argv);
+        break;
+    default:
+        fprintf(stderr, "Opción desconocida: %s\n", argv[1]);
+        exit(EXIT_FAILURE);
     }
-    
+
     exit(EXIT_SUCCESS);
+}
+
+sem_t *sem_operation(char *name, int mode, int value)
+{
+    sem_t *sem;
+    if ((sem = sem_open(name, mode, 0644, value)) == (sem_t *)-1)
+    {
+        perror("No se puede crear o abrir el semáforo");
+        exit(1);
+    }
+    return sem;
 }
